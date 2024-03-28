81999f57f86ee7137aa72141ec7113e3d62ccf79
Dmitry <dkonchik@gmail.com>
2024-03-28 19:01:37 +0300

    Practice on lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
+
+//Задача которую делалали на заятии (готовая)
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {0, 1, 2, 3, 4, 6, 7, 5};
+        int n = 8;
+        Arrays.sort(arr);
+        int num = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > i) {
+                num = arr[i] - 1;
+                break;
+            }
+        }
+
+        if (arr[arr.length - 1] != n) {
+            num = n;
+        }
+
+        System.out.println(num);
     }
 }
